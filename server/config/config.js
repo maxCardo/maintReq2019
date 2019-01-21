6429ecfb838d12b0fc2d39b05117b3d58cf0c398
Scott Carroll <scottcarrollmba@gmail.com>
2019-01-21 10:41:04 -0500

    Committing in order to pull

---
+++ server/config/config.js
@@ -0,0 +1,6 @@
+const twlsid = 'ACdfbd94cac9f215ffe040ff51df27c497';
+const twlAuthToken = '0cf2a7b535363b6539fcd9d2574384b3';
+const gmlAcct = 'scottcarrollmba@gmail.com';
+const gmlPass = 'c@RDB0@RD';
+
+module.exports = {twlAuthToken, twlsid, gmlAcct, gmlPass};
